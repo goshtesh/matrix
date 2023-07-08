56f8f45b34412068dea3ef915b44bb85de3de630
janani01 <jananianantharaman@gmail.com>
2023-07-08 13:49:10 -0700

    Revert "Merge branch 'develop' into UI_upgrades"
    This reverts commit 9d3dd2d373a895c8d5ad09df2cbc1c1ca01ff59c, reversing
    changes made to a3a3d112a77f700a3d3c3d23304910a040d2f454.

--- popup.js
+++ popup.js
@@ -3,8 +3,7 @@ function popup() {
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
         var activeTab = tabs[0];
         var connectFilter = document.getElementById("Connect-filter").value;
-        var connectionCount = document.getElementById("ConnectionCount").value;
-        chrome.tabs.sendMessage(activeTab.id, { "message": "connectRequest", "connectFilter": connectFilter, "connectionCount": connectionCount });
+        chrome.tabs.sendMessage(activeTab.id, { "message": "connectRequest", "connectFilter": connectFilter });
     });
 }
 
@@ -14,7 +13,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function showDiv() {
     document.getElementById('SelectOptions').style.display = "block";
-    document.getElementById('ConnectionCount').value = 10;
 }
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById('ConnectAll').addEventListener("click", showDiv);
@@ -24,6 +22,7 @@ function messagepopup() {
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
         var activeTab = tabs[0];
         var message = document.getElementById("MessageAll").value;
+        console.log("message", message)
         chrome.tabs.sendMessage(activeTab.id, { "message": "messageRequest", "text": message });
     });
 }
