b9bae9087cba13d5d2315630985eb011e9d8716f
janani01 <jananianantharaman@gmail.com>
2023-07-08 13:51:53 -0700

    Revert "Revert "Merge branch 'develop' into UI_upgrades""
    This reverts commit 56f8f45b34412068dea3ef915b44bb85de3de630.

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.message === "connectRequest") {
-            connectCodeBlock(request.connectFilter);
+            connectCodeBlock(request.connectFilter, request.connectionCount, 0);
         }
     }
 );
@@ -15,51 +15,80 @@ chrome.runtime.onMessage.addListener(
 );
 
 
-async function connectCodeBlock(filter) {
-    async function getElementsByXpath(xpathToExecute) {
-        var result = [];
-        var nodesSnapshot = document.evaluate(xpathToExecute, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-        for (var i = 0; i < nodesSnapshot.snapshotLength; i++) {
-            result.push(nodesSnapshot.snapshotItem(i));
-        }
-        return result;
-    }
-
-    let all_buttons = await getElementsByXpath("//button/span[text()='Connect']/..");
-    await new Promise(r => setTimeout(r, 5000));
-
-    let connect_buttons = []
-    for (let btn of all_buttons) {
-        connect_buttons.push(btn)
-    }
-
-    await new Promise(r => setTimeout(r, 5000));
-    for (let btn of connect_buttons) {
-        btn.click()
-        await new Promise(r => setTimeout(r, 2000));
-        const sendInvite = document.getElementById("send-invite-modal").innerText
-        if (sendInvite.includes("Your invitation is almost on its way")) {
-            send = await getElementsByXpath("//button[@aria-label='Send now']")
-            send[0].click()
-            await new Promise(r => setTimeout(r, 2000));
-        } else if (sendInvite.includes("How do you know")) {
-            if (filter) {
-                searchFilter = "//button[text()=" + JSON.stringify(filter) + "]"
-            } else {
-                searchFilter = '//button[text()="Other"]'
+async function connectCodeBlock(filter, count, connectCount) {
+    breakme: if (count > 0) {
+        async function getElementsByXpath(xpathToExecute) {
+            var result = [];
+            var nodesSnapshot = document.evaluate(xpathToExecute, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+            for (var i = 0; i < nodesSnapshot.snapshotLength; i++) {
+                result.push(nodesSnapshot.snapshotItem(i));
             }
-            const filterApply = await getElementsByXpath(searchFilter)
-            filterApply[0].click()
-            connect = await getElementsByXpath("//button[@aria-label='Connect']")
-            connect[0].click()
-            await new Promise(r => setTimeout(r, 2000));
-            send = await getElementsByXpath("//button[@aria-label='Send now']")
-            send[0].click()
-            await new Promise(r => setTimeout(r, 2000));
+            return result;
         }
+        async function getElementByXpath(xpathToExecute, docValue) {
+            if (docValue === null) {
+                return document.evaluate(xpathToExecute, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            } else {
+                return document.evaluate(xpathToExecute, docValue, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            }
+        }
+
+        let all_buttons = await getElementsByXpath("//button/span[text()='Connect']/..");
+        await new Promise(r => setTimeout(r, 5000));
+
+        let connect_buttons = []
+        for (let btn of all_buttons) {
+            connect_buttons.push(btn)
+        }
+        await new Promise(r => setTimeout(r, 5000));
+        for (let btn of connect_buttons) {
+            btn.click()
+            await new Promise(r => setTimeout(r, 2000));
+            const sendInvite = document.getElementById("send-invite-modal").innerText
+            if (sendInvite.includes("Your invitation is almost on its way") || sendInvite.includes("You can customize this invitation")) {
+                send = await getElementsByXpath("//button[@aria-label='Send now']")
+                send[0].click()
+
+                await new Promise(r => setTimeout(r, 2000));
+                connectCount = connectCount + 1
+            } else if (sendInvite.includes("How do you know")) {
+                if (filter) {
+                    searchFilter = "//button[text()=" + JSON.stringify(filter) + "]"
+                } else {
+                    searchFilter = '//button[text()="Other"]'
+                }
+                const filterApply = await getElementsByXpath(searchFilter)
+                filterApply[0].click()
+                connect = await getElementsByXpath("//button[@aria-label='Connect']")
+                connect[0].click()
+                await new Promise(r => setTimeout(r, 2000));
+                send = await getElementsByXpath("//button[@aria-label='Send now']")
+                send[0].click()
+                await new Promise(r => setTimeout(r, 2000));
+                connectCount = connectCount + 1
+
+            } else {
+                let close_button = await getElementByXpath("//button[contains(@class, 'artdeco-modal__dismiss')]//li-icon[@type='cancel-icon']", null);
+                close_button.click();
+            }
+            error_message = await getElementsByXpath("//div[contains(@class, 'artdeco-toasts_toasts')]")
+            if (error_message[0].getElementsByClassName('artdeco-toast-item').length > 0) {
+                let close_button = await getElementByXpath("//button[contains(@class, 'artdeco-toast-item__dismiss')]//li-icon[@type='cancel-icon']", null);
+                close_button.click();
+                connectCount = connectCount - 1
+            }
+            if ((Number(connectCount) === Number(count)) || (Number(connectCount) > Number(count))) {
+                break breakme;
+            }
+        }
+        await new Promise(r => setTimeout(r, 1000));
+        next = await getElementByXpath("//div[contains(@class, 'artdeco-pagination')]/button/span[text()='Next']", null)
+
+        await new Promise(r => setTimeout(r, 4000));
+        next.click()
+        await new Promise(r => setTimeout(r, 2000));
+        connectCodeBlock(filter, count, connectCount)
     }
-
-
 }
 
 
