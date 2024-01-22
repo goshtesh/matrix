becfe6b82127a2e9f019eb613c4aa94b792cba13
janani01 <37977051+janani01@users.noreply.github.com>
2024-01-22 22:24:35 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,24 +1,19 @@
-
 function popup() {
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
         var activeTab = tabs[0];
         var connectFilter = document.getElementById("Connect-filter").value;
-        var connectionCount = document.getElementById("ConnectionCount").value;
-        chrome.tabs.sendMessage(activeTab.id, { "message": "connectRequest", "connectFilter": connectFilter, "connectionCount": connectionCount });
+        chrome.tabs.sendMessage(activeTab.id, { "message": "connectRequest", "connectFilter": connectFilter });
     });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("SelectOptions").addEventListener("submit", popup);
-});
-
 function showDiv() {
     document.getElementById('SelectOptions').style.display = "block";
-    document.getElementById('ConnectionCount').value = 10;
 }
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById('ConnectAll').addEventListener("click", showDiv);
-});
+
+function reset() {
+    chrome.storage.local.set({ "contactedPeople": [] });
+}
+
 
 function messagepopup() {
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
@@ -28,9 +23,9 @@ function messagepopup() {
     });
 }
 
-
-
 document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("SelectOptions").addEventListener("submit", popup);
+    document.getElementById('ConnectAll').addEventListener("click", showDiv);
+    document.getElementById('resetButton').addEventListener("click", reset);
     document.getElementById('theForm').addEventListener("submit", messagepopup);
 });
-
